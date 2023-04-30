b22871fd2aaaaaba7fb2784b908b4c8500336e51
Will Vining <wfv@vining.dev>
2023-04-30 17:28:02 -0600

    Test valid constraints on numbers

--- test/jerk_loader_test.erl
+++ test/jerk_loader_test.erl
@@ -31,3 +31,30 @@ unconstrained_property_types_test_() ->
                                 <<"type">> => <<"object">>,
                                 <<"properties">> => Properties}),
                  Expected} || {Properties, Expected} <- Tests]].
+
+number_constraint_test_() ->
+    Constraints = [#{<<"type">> => <<"number">>,
+                     <<"minimum">> => 5,
+                     <<"maximum">> => 10.2},
+                   #{<<"type">> => <<"number">>,
+                     <<"exclusiveMinimum">> => -1.2,
+                     <<"exclusiveMaximum">> => 2}],
+    Expected = [{<<"bar">>, number, [{lb, {inclusive, 5}},
+                                     {ub, {inclusive, 10.2}}]},
+                {<<"bar">>, number, [{lb, {exclusive, -1.2}},
+                                     {ub, {exclusive, 2}}]}],
+    Schemas = [jiffy:encode(
+                 #{<<"$id">> => <<"foo">>,
+                   <<"type">> => <<"object">>,
+                   <<"properties">> => #{<<"bar">> => Constraint}})
+               || Constraint <- Constraints],
+    Properties =
+        [Properties || [{_, object, {Properties, _, _}}]
+                           <- lists:map(fun jerk_loader:load_json/1, Schemas)],
+    LoadedConstraints =
+        [lists:sort(LoadedConstraints) || [{_, _, LoadedConstraints}] <- Properties],
+    ExpectedConstraints =
+        [lists:sort(ExpectedConstraints) || {_, _, ExpectedConstraints} <- Expected],
+    [[?_assertMatch([{<<"bar">>, number, _}], Property) || Property <- Properties],
+     [?_assertEqual(Constraint, Result)
+      || {Constraint, Result} <- lists:zip(ExpectedConstraints, LoadedConstraints)]].
